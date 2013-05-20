3e351e9fb839de50acdf310a15f434ca0ad185e1
App Academy Student <student@appacademy.io>
2013-05-20 11:49:28 -0700

    added display_mines

--- minesweeper.rb
+++ minesweeper.rb
@@ -6,7 +6,7 @@ class Board
   def initialize
     @board_size = 9
     @num_of_mines = 10
-    @board = Array.new(board_size, Array.new(board_size, '*'))
+    @board = Array.new(board_size) { Array.new(board_size) {'*'} }
     @mines = Set.new
     set_mines
     p mines
@@ -15,22 +15,50 @@ class Board
 
   def set_mines
 
-    @num_of_mines.times do
+    until mines.length == @num_of_mines
       x = rand(board_size - 1)
       y = rand(board_size - 1)
-
-      mines << [y,x] unless mines.include? ( [y,x] )
-
+      mines << [y,x] unless mines.include?( [y,x] )
     end
 
   end
 
+  def display_mines
+    mines.each do |mine_pos|
+      y, x = mine_pos.first, mine_pos.last
+      board[y][x] = "B"
+    end
+    display
+  end
 
   def display
     board.each { |row| puts row.join(' ') }
+  end
+
+  def check_coord(coord)
+    if mines.include?(coord)
+      puts "Game Over"
+
+    end
+  end
+
+
+
+
+
+end
+
+class Minesweeper
+  attr_accessor :coord
+  def initialize
 
   end
 
+  def input
+    x = gets.chomp.to_i
+    y = gets.chomp.to_i
+    coord = [x, y]
+  end
 
 
 end
